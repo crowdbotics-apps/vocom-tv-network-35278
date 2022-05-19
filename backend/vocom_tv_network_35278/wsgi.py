76f0484a823fef2f89fc54897e5206b529ddf5d5
Crowdbotics <team@crowdbotics.com>
2022-05-19 02:06:06 +0000

    Update models

--- backend/vocom_tv_network_35278/wsgi.py
+++ backend/vocom_tv_network_35278/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vocom_tv_network_35278.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vocom_tv_network_35278.settings")
 
 application = get_wsgi_application()
