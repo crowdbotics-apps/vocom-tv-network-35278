77b8eba7e455333ed00cc51d247c9d274fe45f13
Crowdbotics <team@crowdbotics.com>
2022-05-19 01:52:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "vocom_tv_network_35278",
+  url: "https://vocom_tv_network_35278.botics.co",
+  api: "https://vocom_tv_network_35278.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
